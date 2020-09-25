fecbed839dcf566796a9ae313a161167a34150be
unknown <zizaartist@gmail.com>
2020-09-25 19:29:22 +0900

    trying to add notification thingy from azure

---
+++ Notifications.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiClick
+{
+    public class Notifications
+    {
+        public static Notifications Instance = new Notifications();
+
+        public NotificationHubClient Hub { get; set; }
+
+        private Notifications()
+        {
+            Hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://clicknamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=EFqN1PSOAu8zrfEfBESF75eI3prFnIVBzvEetAEXnqQ=",
+                                                                            "ClickNotificationHub");
+        }
+    }
+}
