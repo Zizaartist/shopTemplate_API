14cff6e317a6b8ae0a5319193cbce3c3ca0c9f60
Ренат Зиазетдинов <zizaartist@gmail.com>
2020-12-08 15:01:12 +0900

    Added banknote controller

---
+++ Controllers/BanknoteController.cs
@@ -0,0 +1,26 @@
+﻿using ApiClick.Models.EnumModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiClick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanknoteController : ControllerBase
+    {
+        ClickContext _context = new ClickContext();
+
+        // GET: api/Category
+        [Authorize(Roles = "SuperAdmin, Admin, User")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BanknoteCl>>> GetBanknoteCl()
+        {
+            return await _context.BanknoteCl.ToListAsync();
+        }
+    }
+}
